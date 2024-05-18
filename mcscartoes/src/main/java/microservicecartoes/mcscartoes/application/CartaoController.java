3b1ba2e6b581cf6a14cce2a43c6bf5884d9592a0
tomazdalcortivo <p12547254@gmail.com>
2024-05-17 21:52:48 -0300

    feat: metodo getCartoesRendaAte

--- mcscartoes/src/main/java/microservicecartoes/mcscartoes/application/CartaoController.java
+++ mcscartoes/src/main/java/microservicecartoes/mcscartoes/application/CartaoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/cartoes")
 @RequiredArgsConstructor
@@ -25,5 +27,10 @@ public class CartaoController {
         Cartao cartao = request.toModel();
         cartaoService.salvar(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda) {
+        List<Cartao> list = cartaoService.getCartaoRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
     }
 }
