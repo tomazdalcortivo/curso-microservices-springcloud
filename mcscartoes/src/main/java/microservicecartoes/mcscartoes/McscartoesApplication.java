e3f6d8cd18343eabec7611da183838ed70447375
tomazdalcortivo <p12547254@gmail.com>
2024-05-20 17:59:22 -0300

    feat: Subscriber para a fila de emissão de cartões

--- mcscartoes/src/main/java/microservicecartoes/mcscartoes/McscartoesApplication.java
+++ mcscartoes/src/main/java/microservicecartoes/mcscartoes/McscartoesApplication.java
@@ -1,9 +1,11 @@
 package microservicecartoes.mcscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class McscartoesApplication {
 
 	public static void main(String[] args) {
